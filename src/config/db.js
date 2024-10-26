3a95fa621daf6470de54c14aecac9d3bf8e4be21
Ihssanrida <ridaridi@roehampton.ac.uk>
2024-10-26 21:51:15 +0300

    docker configuration

--- src/config/db.js
+++ src/config/db.js
@@ -3,11 +3,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME  || 'world',
+  port: process.env.DB_PORT || 3306
 });
 
 db.connect((err) => {
